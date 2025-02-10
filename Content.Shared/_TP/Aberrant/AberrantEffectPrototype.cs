1f93efed1da5c2d1b3bec78f7566c349da60592b
Lye <zac@doodle.co.uk>
2025-02-10 16:37:26 +0000

    I commit them to the wrong fucking branch

---
+++ Content.Shared/_TP/Aberrant/AberrantEffectPrototype.cs
@@ -0,0 +1,33 @@
+﻿using Robust.Shared.Prototypes;
+
+namespace Content.Shared._TP.Aberrant;
+
+/// <summary>
+/// This is a prototype for...
+/// </summary>
+[Prototype("aberrantEffect")]
+[DataDefinition]
+public sealed partial class AberrantEffectPrototype : IPrototype
+{
+    /// <inheritdoc/>
+    [IdDataField]
+    public string ID { get; private set;} = default!;
+
+    /// <summary>
+    ///     Stores the effects making up this effect. List incase we want compound effects
+    /// </summary>
+    [DataField("effects")]
+    public List<AberrantEffect> Effects= new();
+
+}
+
+[Serializable]
+[DataDefinition]
+public partial struct AberrantEffect
+{
+    [DataField("id")]
+    public EntProtoId? PrototypeId = null;
+
+    [DataField]
+    public List<Component> Components = new();
+}
---
+++ Content.Shared/_TP/Aberrant/AberrantTablePrototype.cs
@@ -0,0 +1,19 @@
+﻿using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._TP.Aberrant;
+
+/// <summary>
+/// A collection of effects that can be randomly applied to an entity when it reaches a certain level of aberrant damage.
+/// </summary>
+[Prototype("aberrantTable")]
+[DataDefinition]
+public sealed partial class AberrantTablePrototype : IPrototype
+{
+    /// <inheritdoc/>
+    [IdDataField]
+    public string ID { get; private set;} = default!;
+
+    [DataField("effects")]
+    public Dictionary<AberrantEffect, float> Effects= new();
+}
